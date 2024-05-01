0b75f9a0fa0ec6bbdc5d89a5ccc69e38e7eedd5c
YI <012attack@naver.com>
2024-05-01 21:37:17 +0900

    Initial commit

---
+++ src/main/java/com/example/basicspringsecurityjwt/BasicSpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicspringsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringSecurityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSpringSecurityJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basicspringsecurityjwt/BasicSpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicspringsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringSecurityJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
